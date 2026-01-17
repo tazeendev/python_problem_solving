4bdd5e4e3bead2aa274e2f220e65faeaabfeca25
tazeen-dev <tazeenzahrabatool@gmail.com>
2026-01-17 20:57:03 +0500

    grade system

---
+++ python_basics/average.py
@@ -0,0 +1,4 @@
+num1=int(input('enter the number :'))
+num2=int(input('enter the number :'))
+average=(num1 + num2)/2
+print('the average of numbers is :' , average)
---
+++ python_basics/grade_calculator.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 85:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
