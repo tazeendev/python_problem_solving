b283b886a6824e47d60e3b8d67eb72baf78adc03
tazeen-dev <tazeenzahrabatool@gmail.com>
2026-07-11 12:37:11 +0500

    linear search

--- python_basics/python_revision/DSA/tempCodeRunnerFile.py
+++ python_basics/python_revision/DSA/tempCodeRunnerFile.py
@@ -3,10 +3,10 @@ def find_paper(papers, name):
         if paper == name:
             return True
     return False
+
 papers = ['Ali', 'Ahmad', 'Safia', 'Sadia', 'Sabah', 'Ahmad']
 search_name = 'Ahmad'
 result = find_paper(papers, search_name)
 if result:
     print(f'{search_name} found in the list')
 else:
-    print(f'{search_name} not found in the list') 
--- python_basics/shawana_python/newpython.py
+++ python_basics/shawana_python/newpython.py
@@ -1,2 +1,8 @@
 print('hello world')
 print('aaaaaaaaa')
+if 5>3:
+    print('hello')
+else:
+    print('no')
+    if 4>2:
+        
