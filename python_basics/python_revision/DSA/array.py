42a7df4d4284d4329d8f8927a37cb11168bdcaf6
tazeen-dev <tazeenzahrabatool@gmail.com>
2026-07-12 13:21:07 +0500

    dsa problems

---
+++ python_basics/python_revision/DSA/array.py
@@ -0,0 +1,13 @@
+num=[20,30,40,50,60,70,80]
+print(num)
+num2=num.append(67)
+print(num2)
+marks=[60,65,70,89,70,45,90,99]
+for i in marks:
+    print(i)
+for i in range(len(marks)):
+    print(marks[i])
+for i in reversed(marks):
+    print(i)
+for i in range(len(marks) -1,-1,-1):
+    print(marks[i])
---
+++ python_basics/shawana_python/check_number.py
@@ -0,0 +1,7 @@
+check_num=int(input('Enter the number'))
+if check_num>0:
+    print('Positive number')
+elif check_num<0:
+    print('Negative number')
+else:
+    print('zero number')
